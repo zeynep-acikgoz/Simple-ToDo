e5dd5af64157425543dbe6b790b52be8c7961b24
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2025-11-23 22:03:24 +0300

    update

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -2,5 +2,17 @@
 
 public class TodoItem
 {
+    public int Id {get; set;}
+    public string? Title {get; set;}
+    public DateTime DueDate { get; set; }
+    public DateTime CreatedDate {get; set;}
+    public bool IsComplete { get; set; }
+
+    public TodoItem()
+    {
+        CreatedDate = DateTime.Now;
+        IsComplete = false;
+        Title = "";
+    }
     
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,23 +1,45 @@
-﻿namespace CetTodoApp;
+﻿using CetTodoApp.Data;
+
+namespace CetTodoApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+   
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        FakeDb.AddToDo("Test1" ,DateTime.Now.AddDays(-1));
+        FakeDb.AddToDo("Test2" ,DateTime.Now.AddDays(1));
+        FakeDb.AddToDo("Test3" ,DateTime.Now);
+        RefreshListView();
+        ;
 
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+    private void AddButton_OnClicked(object? sender, EventArgs e)
+    {
+        FakeDb.AddToDo(Title.Text, DueDate.Date);
+        Title.Text = string.Empty;
+        DueDate.Date=DateTime.Now;
+        RefreshListView();
+    }
+
+    private void RefreshListView()
+    {
+        TasksListView.ItemsSource = null;
+        TasksListView.ItemsSource = FakeDb.Data.Where(x => !x.IsComplete ||
+                                                           (x.IsComplete && x.DueDate > DateTime.Now.AddDays(-1)))
+            .ToList();
+    }
+
+    private void TasksListView_OnItemSelected(object? sender, SelectedItemChangedEventArgs e)
+    {
+        var item = e.SelectedItem as TodoItem;
+       FakeDb.ChageCompletionStatus(item);
+       RefreshListView();
+       
+    }
 }
